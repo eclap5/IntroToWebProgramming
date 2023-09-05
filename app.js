67253b0583465bd53cbdff02d9bf0b2ad97355b5
Aleksi Haapalainen <aleksi.haapalainen@student.lut.fi>
2023-09-06 00:55:07 +0300

    Week 2 done

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const username = document.getElementById('input-username')
 const email = document.getElementById('input-email')
 const admin = document.getElementById('input-admin')
 const table = document.getElementById('table')
+const image = document.getElementById('input-image')
 
 
 const submitForm = async (event) => {
     let entryUsername = document.createElement('td').textContent
     let entryEmail = document.createElement('td').textContent
     let entryAdmin = document.createElement('td')
+    let entryImage = document.createElement('img')
 
     entryUsername = username.value
     entryEmail = email.value
     entryAdmin = '-'
+
+    if (image.files[0]) {
+        entryImage.src = URL.createObjectURL(image.files[0])
+        entryImage.width = 64
+        entryImage.height = 64    
+    }
 
     if (admin.checked === true) {
         entryAdmin = 'X'
@@ -22,6 +30,7 @@ const submitForm = async (event) => {
         row.insertCell().innerHTML = entryUsername
         row.insertCell().innerHTML = entryEmail
         row.insertCell().innerHTML = entryAdmin
+        row.insertCell().appendChild(entryImage)
     }
 
     event.preventDefault()
@@ -39,9 +48,9 @@ const clearData = () => {
 const findEntry = (entryUsername, entryEmail, entryAdmin) => {
     for (let i = 1; i < table.rows.length; i++) {
         cells = table.rows[i].cells
-        if (cells[0].firstChild.textContent === entryUsername) {
-            cells[1].firstChild.textContent = entryEmail
-            cells[2].firstChild.textContent = entryAdmin
+        if (cells[0].textContent === entryUsername) {
+            cells[1].textContent = entryEmail 
+            cells[2].textContent = entryAdmin
             return true
         }
     }
