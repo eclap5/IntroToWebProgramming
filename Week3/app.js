675e27a37b708d8401c119ce57701d74cef97905
Aleksi Haapalainen <aleksi.haapalainen@student.lut.fi>
2023-09-14 15:12:18 +0300

    exercise 4 added

--- Week3/app.js
+++ Week3/app.js
@@ -5,29 +5,40 @@ const fetchData = async () => {
 }
 
 
-const populateTable = () => {
-    fetchData()
-    .then(data => {
-        let tableBody = document.getElementById('table-body');
-        dataMapper(data, tableBody);
-    });
+const fetchEmploymentData = async () => {
+    const response = await fetch('https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065');
+    const employmentData = await response.json();
+    return employmentData;
 }
 
 
-const dataMapper = (data, tableBody) => {
+const populateTable = async () => {
+    const data = await fetchData();
+    const employmentData = await fetchEmploymentData();
+    
+    let tableBody = document.getElementById('table-body');
+    dataMapper(data, tableBody, employmentData);
+}
+
+
+const dataMapper = (data, tableBody, employmentData) => {
     let municipality = data.dataset.dimension.Alue.category.label;
     let values = data.dataset.value;
+    let employment = employmentData.dataset.value;
     
     for (let i = 0; i < values.length; i++) {
         let row = document.createElement('tr');
         let municipalityCell = document.createElement('td');
         let valueCell = document.createElement('td');
+        let employmentCell = document.createElement('td');
         
         municipalityCell.innerHTML = Object.values(municipality)[i];
         valueCell.innerHTML = values[i];
+        employmentCell.innerHTML = employment[i];
 
         row.appendChild(municipalityCell);
         row.appendChild(valueCell);
+        row.appendChild(employmentCell);
         tableBody.appendChild(row);
     }
 }
